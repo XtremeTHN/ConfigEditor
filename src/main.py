e7699f12b0f9756710fd9e7359c6d5c41b749ca8
Axel <nigthmaresans2@gmail.com>
2024-02-21 08:27:50 -0600

    Parsers import working

--- src/main.py
+++ src/main.py
@@ -1,1 +1,8 @@
+from modules.parser import UserParsers
+import icecream
+user = UserParsers()
+user.list_parsers()
 
+parser = user.get_parser("hyprland")
+
+print(parser.filetype, parser.filename)
--- src/modules/parser.py
+++ src/modules/parser.py
@@ -1,8 +1,19 @@
 import os
+import sys
 import pathlib
 import json
 
+import icecream
 import importlib.util
+
+class Format:
+    end = '\033[0m'
+    underline = '\033[4m'
+    bold = '\033[1m'
+
+    class Colors:
+        red = '\u001b[31m'
+        light_red = "\033[1;31m"
 
 PARSER_SHARE_PATH=pathlib.Path(os.path.join(os.environ['HOME'], ".config", "configeditor"))
 DEFAULT_PARSER_CONFIG={
@@ -12,11 +23,20 @@ if PARSER_SHARE_PATH.exists() is False:
     PARSER_SHARE_PATH.mkdir(parents=True,exist_ok=True)
 
 class Parser:
+    filetype=""
+    filename=""
     def __init__(self, file):
         ...
 
     def parse(self):
         ...
+
+def warn(*args):
+    print(Format.Colors.light_red, *args, Format.end)
+
+def error(*args, exit_code=1):
+    print(Format.Colors.red, *args, Format.end)
+    sys.exit(exit_code)
 
 class UserParsers:
     def __init__(self):
@@ -26,13 +46,38 @@ class UserParsers:
 
         self.config = json.loads(self.parser_conf_path.read_text())
     
-    def get_parser(self, name) -> Parser:
-        for parsers_conf_dict in self.config['parsers']:
-            for parser_conf in parsers_conf_dict:
-                if (n:=parser_conf["name"]) == name:
-                    spec = importlib.util.spec_from_file_location(n, parser_conf["path"])
-                    module = importlib.util.module_from_spec(spec)
+    def get_parser(self, name) -> Parser | None:
+        for parser_conf in self.config["parsers"]:
+            if (n:=parser_conf["name"]) == name:
+                module_path = parser_conf.get("path")
+                if module_path is None:
+                    module_path = PARSER_SHARE_PATH / "scripts" / f"{n}.py"
 
+                spec = importlib.util.spec_from_file_location(n, module_path)
+                module = importlib.util.module_from_spec(spec)
+                try:
                     spec.loader.exec_module(module)
+                except FileNotFoundError:
+                    error(f'"{module_path}" does not exist, if this is not the path of the script, you will need to specify the patb on config')
+                parser_obj: Parser = getattr(module, "Parser")
+                if parser_obj is None:
+                    warn("The module", n, "doesn't have the class Parser")
+                    return
+                if parser_conf.get("file") is None:
+                    error("Parser", n, "has no file section on the parsers.conf")
+                if parser_conf["file"].get("name") is None:
+                    error("Parser has the file section incomplete, missing name key on file")
+                if parser_conf["file"].get("type") is None:
+                    error("Parser", n, "has the file section incomplete, missing type key")
 
-                    return module
+                parser_obj.filename = parser_conf["file"]["name"]
+                parser_obj.filetype = parser_conf["file"]["type"]
+                
+                return parser_obj
+    def list_parsers(self):
+        print(f"{Format.underline + Format.bold}Available parsers:{Format.end}")
+        for parser_conf in self.config["parsers"]:
+            if (name:=parser_conf.get("name")) is not None:
+                print(f"\t{name}")
+            else:
+                print("\tNo parser name")
